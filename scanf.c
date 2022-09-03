073a080fc5a0bf028d6215a623832140b5b78512
DjTekos <112503968+DjTekos@users.noreply.github.com>
2022-09-02 23:08:09 -0300

    Add files via upload

---
+++ scanf.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main(){
+
+int idade=0;
+float peso=0.0;
+
+printf("Bem-vindo, vamos fazer o seu cadastro!\n");
+
+printf("Digite uma idade\n");
+
+scanf("%d",&idade);
+
+printf("Agora digite um peso\n");
+
+scanf("%f",&peso);
+
+
+printf("Idade informada:%d.\n",idade);
+printf("Peso informado:%f.\n",peso);
+
+printf("Cadastro concluido, muito obrigado!");
+	
+}
+
