6c958d62e56d72eaddf1ef84e226d560445e9e56
DjTekos <112503968+DjTekos@users.noreply.github.com>
2022-09-03 11:57:25 -0300

    Add files via upload

---
+++ operacoes aritimeticas.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(){
+	
+int A,B,soma,subt,mult,divi;
+
+printf("Digite um valor para fazer o calculo.\n");
+scanf("%d",&A);
+
+printf("Digite o segundo valor.\n");
+scanf("%d",&B);
+
+soma=A+B;
+subt=A-B;
+mult=A*B;
+divi=A/B;
+
+printf("Resultados:\n");
+printf("soma:%d.\n",soma);
+printf("subt:%d.\n",subt);
+printf("mult:%d.\n",mult);
+printf("divi:%d.\n",divi);
+
+	
+	
+}
