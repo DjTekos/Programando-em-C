ff14bed699968229853e7441c721526880d14055
DjTekos <112503968+DjTekos@users.noreply.github.com>
2022-09-07 10:37:41 -0300

    Add files via upload

---
+++ escrever a tabuada de 1 a 10 de um numero de 1 a 10.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(){
+
+int a,b,result;
+
+printf("Digite um numero de 1 a 10");
+scanf("%d",&a);
+
+if(a<=10&&a<=1)
+{
+for(b=1;b<=10;b<=1;b++)	
+}
+result=(a*b);
+printf("\t%d*%d=%d\n",a,b,result);	
+}
+else
+printf("Numero invalido!\n");
+
+}
